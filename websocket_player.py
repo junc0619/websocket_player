25837087f9aa41e64622144d3f830f8f3d337e33
junc0619 <junc0619@gmail.com>
2018-04-29 17:48:59 -0700

    2nd commit

--- websocket_player.py
+++ websocket_player.py
@@ -1,22 +1,31 @@
 #import vlc
 import mplayer
 import subprocess
-from websocket import create_connection
+# websocket-client package
+import websocket
 
+ws_url="192.168.2.132"
+ws_port=9080
 
-ws = create_connection("ws://localhost:9080") #, subprotocols=["binary", "base64"])
+websocket.enableTrace(True)
+ws_resource="ws://"+ws_url+ ":" + str(ws_port)
+print ("Connecting websocket server: "+ ws_resource)
+ws = websocket.create_connection(ws_resource) #, subprotocols=["binary", "base64"])
+
+print ("Creating and opening audio.webm file...")
 f = open('audio.webm', 'ab')
 
 #cmdline = ['cvlc',   '--demux', 'h264', '-'] #pick a media player
 #cmdline = ['mplayer', '-noconsolecontrols',  '-fps', '25', '-cache', '1024', '-']
 cmdline = ['mplayer', '-noconsolecontrols',  '-fps', '25', '-']
-
 print (cmdline)
+
 player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
-print ("player opened")
+print ("Player was opened in subprocess...")
+print ("Receiving data via websocket...")
 while True:
     data = ws.recv()
-    print ("in loop")
+    #print ("in loop")
     if data:
        f.write(data)
 
